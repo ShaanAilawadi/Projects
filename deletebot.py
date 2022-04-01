6facd7c0200818cfddf6288e65e311a230ea62e6
Shaan Ailawadi <ailawadis04@gmail.com>
2022-03-31 22:30:09 -0600

    pls use this its done

--- deletebot.py
+++ deletebot.py
@@ -1,18 +1,26 @@
 import discord
+import os
+import requests
+import json
 
+# makes discord usable
 client = discord.Client()
-
 
 @client.event
 async def on_message(message):
-    # users message
+
+    # members message
     msg = message.content
 
-    if msg.startswith('https://'):
-        msg.delete()
+    # establishes channel and deletes link
+    if str(message.channel) == 'general' and msg.startswith("https://"):
+        await message.channel.purge(limit=1)
 
-    elif msg.startswith('http'):
-        msg.delete()
+    # establishes channel and deletes link
+    if str(message.channel) == 'general' and message.content.startswith("http"):
+        await message.channel.purge(limit=1)
 
- if __name__ == '__main__':
-    client.run()
+
+if __name__ == '__main__':
+    # runs the bot
+    client.run("OTU5Mjk2ODQzNTAwNjM4MjA4.YkZ0zg.A7H_wlId1u9IR3NMG3mO4c5EZi0")
